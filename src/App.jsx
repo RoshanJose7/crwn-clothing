61a83d2f3accaff8643a6d87e8cdb391f989ffdc
RoshanJose7 <roshanstark007@gmail.com>
2020-09-21 21:46:58 +0530

    Add: React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import HomePage from "./pages/homePage/homePage.component";
+import { Route } from "react-router-dom";
 import "./App.scss";
+
+const TopicList = () => <h1>Topic List Page</h1>;
+const TopicDetail = () => <h1>Topic Detail Page</h1>;
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route path="/" component={HomePage} exact />
+      <Route path="/topic" component={TopicList} />
+      <Route path="/topic:topicId" component={TopicDetail} />
     </div>
   );
 }
--- src/pages/homePage/homePage.component.jsx
+++ src/pages/homePage/homePage.component.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import DirectoryMenu from "../../components/directory-menu/directoryMenu.component";
 import "./homepage.styles.scss";
 
-const HomePage = () => {
+const HomePage = (props) => {
+  console.log(props);
   return (
     <div className="homepage">
       <DirectoryMenu />
