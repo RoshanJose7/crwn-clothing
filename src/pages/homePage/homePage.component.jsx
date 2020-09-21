16fa5e8fced8acd5d6304521713c71152021e9eb
RoshanJose7 <roshanstark007@gmail.com>
2020-09-21 14:06:49 +0530

    Add: Home Page UI Add: Menu Items

---
+++ src/pages/homePage/homePage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DirectoryMenu from "../../components/directory-menu/directoryMenu.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  );
+};
+
+export default HomePage;
